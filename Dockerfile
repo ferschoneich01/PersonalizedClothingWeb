a31e3ab05960bf5e16d3dac13fd8bfb73c99f904
ferschoneich01 <ferschoneich@gmail.com>
2023-04-19 15:15:32 +0600

    test deploy full

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN sudo apt install libpq-dev python3-dev
 
 # syntax=docker/dockerfile:1
 FROM python:3
-RUN pip install psycopg2-binary 
+RUN pip install psycopg2 
 
 FROM alpine:3.10
 
--- src/funciones.py
+++ src/funciones.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
