8145bde21130337eec85a41254a9e5844981f66c
ferschoneich01 <59126953+ferschoneich01@users.noreply.github.com>
2023-04-25 16:00:19 +0000

    Tue, Apr 25, 2023, 10:00 PM +06:00

--- src/static/js/ads.js
+++ src/static/js/ads.js
@@ -5,10 +5,29 @@ overlay.className = "overlay";
 document.body.appendChild(overlay);
 var close = document.getElementsByClassName("close")[0];
 
-window.onload = function () {
-    modal.style.display = "block";
-    overlay.style.display = "block";
-}
+
+function seHaMostradoHoy() {
+    const hoy = new Date();
+    const fechaMostrada = localStorage.getItem('fechaMostrada');
+    if (fechaMostrada === null) {
+      return false;
+    }
+    const fecha = new Date(fechaMostrada);
+    return fecha.getDate() === hoy.getDate() &&
+           fecha.getMonth() === hoy.getMonth() &&
+           fecha.getFullYear() === hoy.getFullYear();
+  }
+
+  /* Mostrar la ventana modal si no se ha mostrado hoy */
+  if (!seHaMostradoHoy()) {
+    window.onload = function () {
+        modal.style.display = "block";
+        overlay.style.display = "block";
+    }
+    localStorage.setItem('fechaMostrada', new Date().toISOString());
+  }
+
+
 
 close.onclick = function () {
     modal.style.display = "none";
