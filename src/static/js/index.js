eeff6ec0be82c45534938d450a93e6872909b8bb
ferschoneich01 <59126953+ferschoneich01@users.noreply.github.com>
2023-04-25 07:13:53 +0000

    Tue, Apr 25, 2023, 1:13 PM +06:00

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,21 +1,28 @@
 
 //anuncios
 var modal = document.getElementById("modal");
+var overlay = document.createElement("aside");
+overlay.className = "overlay";
+document.body.appendChild(overlay);
 var close = document.getElementsByClassName("close")[0];
 
 window.onload = function () {
     modal.style.display = "block";
+    overlay.style.display = "block";
 }
 
 close.onclick = function () {
     modal.style.display = "none";
+    overlay.style.display = "none";
 }
 
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        overlay.style.display = "none";
     }
 }
+
 
 
 // script del menu responsive Abrir el menu
